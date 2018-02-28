464e831bfb5f42c04f97edf7d1fd3aae4b3372f9
Patit <patit@isisdsn.net>
2018-02-28 20:46:48 +0530

    First Commit

--- js/controller/dashboard-blank.controller.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module("app");
-app.controller("dashboardCtrl",['$scope','dashboardService','loginService',function($scope,dashboardService,loginService){
-	loginService.checkProtected(function(res){
-		console.log(res);
-	});
-}]);
